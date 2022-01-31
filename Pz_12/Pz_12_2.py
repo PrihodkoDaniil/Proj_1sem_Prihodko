4bd62110ff049c4113811eed68ee9489d9e6caf0
Student <pdm20042203@gmail.com>
2022-01-31 18:06:29 +0300

    Добавлена Пз 12

--- Pz_12/Pz_12_2.py
+++ Pz_12/Pz_12_2.py
@@ -5,17 +5,13 @@ root = Tk()
 root.geometry('500x100')
 
 def var():
-    a=num.get()
-    while True:
-        if 10 < a < 100 and (a % 2) == 0:
-            print('Истинно')
-        else:
-            print('Ложно')
-    final_0={print}
-Label(text='Ввод числа: ').grid(row=1, column=0)
+    strof=num.get()
+    strof.upper()
+    final_0 =f"{strof}"
+Label(text='Ведите текст: ').grid(row=1, column=0)
 num=Entry(width='50')
 num.grid(row=1,column=1,sticky='w')
-Button(text='Узнать истинно или ложно',bg='yellow',activebackground='yellow',
+Button(text='Сделать все буквы заглавными',bg='yellow',activebackground='yellow',
        command=var).grid(row=2,column=1,sticky='w')
 Label(text='Результат:').grid(row=3,column=0)
 final_0=Label()
