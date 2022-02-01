673cc7d7c3c4320ba3514cab54b7e2dbfa76a3a4
PrihodkoDaniil <pdm20042203@gmail.com>
2022-02-01 09:21:02 +0300

    Добавлена пз 12

--- Pz_12/Pz_12_2.py
+++ Pz_12/Pz_12_2.py
@@ -1,19 +1,25 @@
 #Разработать программу с применением пакета tk, взяв в качестве условия одну любую задачу из ПЗ №№ 3 – 8.
+#Была выбрана задача 7_1
+#Условие:
+#Преобразовать в строке все строчные буквы(как латинские, так и русские) в прописные.
 from tkinter import *
 
-root = Tk()
-root.geometry('500x100')
 
-def var():
-    strof=num.get()
-    strof.upper()
-    final_0 =f"{strof}"
+
+def var(vod):
+    te['text'] = 'Измененная строка: ',num.get().upper()
+
+
+root = Tk()
+root.title('Преобразование всех строчных букв в прописные')
+root.geometry('500x100')
 Label(text='Ведите текст: ').grid(row=1, column=0)
 num=Entry(width='50')
-num.grid(row=1,column=1,sticky='w')
-Button(text='Сделать все буквы заглавными',bg='yellow',activebackground='yellow',
-       command=var).grid(row=2,column=1,sticky='w')
+num.grid(row=1,column=1)
+button1 = Button(text='Сделать все буквы заглавными',bg='yellow',activebackground='yellow')
+button1.grid(row=2,column=1)
 Label(text='Результат:').grid(row=3,column=0)
-final_0=Label()
-final_0.grid(row=3,column=1,stick='w')
+te=Label(font='arial 12')
+te.grid(row=3,column=1)
+button1.bind('<Button-1>',var)
 root.mainloop()
