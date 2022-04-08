61078571fa9e4ebc644b23b228631c1500c5620b
PrihodkoDaniil <pdm20042203@gmail.com>
2022-04-08 17:56:43 +0300

    Добавлена пз 15

--- Pz_15/Pz_15_1.py
+++ Pz_15/Pz_15_1.py
@@ -5,11 +5,9 @@ i = int(input("Введите количество строк: "))
 j = int(input("Введите количество столбцов: "))
 B = [[randint(-10,10) for y in range(j)] for x in range(i)]
 print('Начальная матрица')
-for i in B:
-    print(*i)
+for i in B:print(*i)
 for i in range(len(B)):
     for j in range(len(B[i])-1):
         B[i][j+1] += B[i][j]
 print('Матрица по условию')
-for i in B:
-    print(*i)
+for i in B:print(*i)
--- Pz_15/Pz_15_2.py
+++ Pz_15/Pz_15_2.py
@@ -4,6 +4,5 @@ i = int(input("Введите количество строк: "))
 j = int(input("Введите количество столбцов: "))
 matrix = [[randint(-10,10) for y in range(j)] for x in range(i)]
 print('Матрица')
-for i in matrix:
-    print(*i)
+for i in matrix:print(*i)
 print("Сумма первых двух строк:",sum(matrix[0])+sum(matrix[1]))
