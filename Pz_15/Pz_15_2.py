63a688b15c225a038e2104b01152bbccca45f02d
PrihodkoDaniil <pdm20042203@gmail.com>
2022-04-08 07:39:50 +0300

    Добавлена пз 15

--- Pz_15/Pz_15_2.py
+++ Pz_15/Pz_15_2.py
@@ -3,6 +3,7 @@ from random import randint
 i = int(input("Введите количество строк: "))
 j = int(input("Введите количество столбцов: "))
 matrix = [[randint(-10,10) for y in range(j)] for x in range(i)]
+print('Матрица')
 for i in matrix:
     print(*i)
 print("Сумма первых двух строк:",sum(matrix[0])+sum(matrix[1]))
